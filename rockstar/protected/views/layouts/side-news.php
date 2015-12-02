1b2973563972486f2f5457c63b1df3d199d179d9
rangga rahadian <rangga rahadian.tifd13@polban.ac.id>
2015-12-02 23:27:55 +0700

    styling timeline
    penambahan kolom news (dummy)

---
+++ rockstar/protected/views/layouts/side-news.php
@@ -0,0 +1,57 @@
+<div class="col-md-6">
+	<div class="panel" style="min-width:160px;">
+        <div class="panel-heading text-center" style="background-color:#555;color:#eee;">NEWS</div> 
+            <div class="panel-body" >
+            	<hr>
+            		<div>
+            			<table class="table" border=0 align="center"> 
+				            <style>
+				                    img {
+				                      float: left;
+				                    }
+				            </style> 
+				            <?php
+				            	$info = "lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet lorem ipsum sit dolor amet";
+				            ?>
+				            
+				            <tr>
+				                <td class="col-md-6">
+				                	<p><img src ="<?php echo Yii::app()->request->baseUrl; ?>/media/a5.jpg" style="width:100%;"></p>
+				                </td>
+				                <td class="col-md-6">
+				                	<h3><b>Lorem Ipsum 1</b><h3>
+				                	<h5><?php echo $info ?></p></h5>
+				                	<a href=""><i>Read More</i></a> 
+				                </td>						 
+						 	</tr>		
+						 	<hr>
+
+				            <tr>
+				                <td class="col-md-6">
+				                	<p><img src ="<?php echo Yii::app()->request->baseUrl; ?>/media/b2.jpg" style="width:100%;"></p>
+				                </td>
+				                <td class="col-md-6">
+				                	<h3><b>Lorem Ipsum2</b><h3>
+				                	<h5><?php echo $info ?></p></h5>
+				                	<a href=""><i>Read More</i></a> 
+				                </td>						 
+						 	</tr>		
+						 	<hr>
+
+				            <tr>
+				                <td class="col-md-6">
+				                	<p><img src ="<?php echo Yii::app()->request->baseUrl; ?>/media/b6.jpg" style="width:100%;"></p>
+				                </td>
+				                <td class="col-md-6">
+				                	<h3><b>Lorem Ipsum3</b><h3>
+				                	<h5><?php echo $info ?></p></h5>
+				                	<a href=""><i>Read More</i></a> 
+				                </td>						 
+						 	</tr>		
+						 	<hr>
+				    	</table>
+            		</div>
+                </div>
+        </div>
+    </div>
+</div>
--- rockstar/protected/views/timeline/index.php
+++ rockstar/protected/views/timeline/index.php
@@ -8,57 +8,55 @@
 	<!-- left side column -->
 	<!--mid column-->
 	
-	<?php $this->renderpartial('../layouts/side-komunitas');  ?>
+	<?php $this->renderpartial('../layouts/side-news');  ?>
 	<!-- right content column-->
-	<div class="col-md-7" >
+	<div class="col-md-6" >
 		<div class="panel" style="min-width=500px;">
-			<div class="panel-heading text-center" style="background-color:#111;color:#fff;">UPLOAD GALLERY PRIBADI</div>   
+			<div class="panel-heading text-center" style="background-color:#111;color:#fff;">TIMELINE</div>   
 			<div class="panel-body">
 				<br>
 				<div class="row">
 					<div class="col-sm-offset-1 col-sm-10" style="border: 1px solid #ccc;">
 
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
-<hr>
+					<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+					<hr>
 
-<?php 
-foreach ($status as $stat) {?>
-<div style="border:1px solid #ccc;padding:10px;overflow:auto;"> 
-	<div style="float:left;width: 80%;word-wrap: break-word;">
-		<?=  '<b>'.$stat['NAMA_LENGKAP'].' </b><br>'.$stat['KONTEN'] ?>
-	</div>
-	<div style="float:right">
-		<i><?= $stat['DATETIME_STATUS'] ?></i>
-	</div>
-	<hr>
-	<div>
-		<a href="#"> Like </a>
-		<form>
-			<textarea></textarea>
-			<input type="submit" value="Komentar">
-		</form>
-	</div>
-</div> <br>
+					<?php 
+					foreach ($status as $stat) {?>
+					<div style="border:1px solid #ccc;padding:10px;overflow:auto;"> 
+						<div style="float:left;width: 80%;word-wrap: break-word;">
+							<?=  '<b>'.$stat['NAMA_LENGKAP'].' </b><br>'.$stat['KONTEN'] ?>
+						</div>
+						<div style="float:right">
+							<i><?= $stat['DATETIME_STATUS'] ?></i>
+						</div>
+						<hr>
+						<div>
+							<a href="#"> Like </a>
+							<form>
+								<textarea></textarea>
+								<input type="submit" value="Komentar">
+							</form>
+						</div>
+					</div> <br>
 
-<? } 
+					<? } 
 
-//$this->widget('zii.widgets.CListView', array(
-// 	'dataProvider'=>$dataProvider,
-// 	'itemView'=>'_view',
-// 	 'sortableAttributes'=>array(
-//             'name'=>'ID_STATUS',
-//         ),
-// ));
-?>
-</div>
+					//$this->widget('zii.widgets.CListView', array(
+					// 	'dataProvider'=>$dataProvider,
+					// 	'itemView'=>'_view',
+					// 	 'sortableAttributes'=>array(
+					//             'name'=>'ID_STATUS',
+					//         ),
+					// ));
+					?>
+					</div>
 						</div><!--/panel-body-->
 					</div><!--/panel-->
 					<!--/end right column-->
 					<Br>
 					</div> 
 				</div>
-
-				<?php $this->renderpartial('../layouts/side-social-feed');  ?>
 			</div>
 			<!-- </div> -->
 			<!-- </div> -->
